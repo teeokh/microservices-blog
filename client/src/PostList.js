ce671a27bdebf110a01470a43c20a27a048a47db
Terrell <139244119+teeokh@users.noreply.github.com>
2024-09-14 12:25:40 +0100

    Create ingress controller and update localhost routings via alias

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -8,7 +8,7 @@ const PostList = () => {
     const [posts, setPosts] = useState({})
 
     const fetchPosts = async () => {
-        const res = await axios.get('http://localhost:4002/posts')
+        const res = await axios.get('http://blog.com:4002/posts')
 
         setPosts(res.data) // Updates posts with data from res.data (we get back an object of posts)
     }
